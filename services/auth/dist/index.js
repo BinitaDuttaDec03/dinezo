dd084496bd72efb2674895c6dd7ea62c046247fa
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-03-08 09:51:52 +0530

    mongo connection

--- services/auth/dist/index.js
+++ services/auth/dist/index.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import express from "express";
+import connectDB from "./config/db.config.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Auth service is running on port ${PORT}`);
+    connectDB();
 });
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import connectDB from "./config/db.config.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Auth service is running on port ${PORT}`);
+  connectDB();
 });
