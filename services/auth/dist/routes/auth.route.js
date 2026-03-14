e99552e360d0731f06f6d652699e7e5ef6ffe4bb
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-03-14 09:59:14 +0530

    user role route added

--- services/auth/dist/routes/auth.route.js
+++ services/auth/dist/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { loginUser } from "../controllers/auth.controller.js";
+import { addUserRole, loginUser } from "../controllers/auth.controller.js";
+import { isAuthenticated } from "../middlewares/protectRoute.middleware.js";
 const router = express.Router();
 router.post("/login", loginUser);
+router.put("/add/role", isAuthenticated, addUserRole);
 export default router;
