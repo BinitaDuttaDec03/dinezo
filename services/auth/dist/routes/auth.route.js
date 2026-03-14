0898af91470fa1625f07a2e1d1648970a274b0bd
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-03-14 10:04:20 +0530

    my profile route added

--- services/auth/dist/routes/auth.route.js
+++ services/auth/dist/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addUserRole, loginUser } from "../controllers/auth.controller.js";
+import { addUserRole, loginUser, myProfile, } from "../controllers/auth.controller.js";
 import { isAuthenticated } from "../middlewares/protectRoute.middleware.js";
 const router = express.Router();
 router.post("/login", loginUser);
 router.put("/add/role", isAuthenticated, addUserRole);
+router.get("/me", isAuthenticated, myProfile);
 export default router;
--- services/auth/src/routes/auth.route.ts
+++ services/auth/src/routes/auth.route.ts
@@ -1,11 +1,16 @@
 import express from "express";
 
-import { addUserRole, loginUser } from "../controllers/auth.controller.js";
+import {
+  addUserRole,
+  loginUser,
+  myProfile,
+} from "../controllers/auth.controller.js";
 import { isAuthenticated } from "../middlewares/protectRoute.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.put("/add/role", isAuthenticated, addUserRole);
+router.get("/me", isAuthenticated, myProfile);
 
 export default router;
