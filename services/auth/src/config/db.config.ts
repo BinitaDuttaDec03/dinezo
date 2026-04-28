206c01aa162a1b79c092bd8fd1c8e1d541dd308f
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-04-28 07:17:44 +0530

    db configuration

---
+++ services/auth/src/config/db.config.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string, {
+            dbName: "dinezo"
+        })
+        console.log("Connected to DB")
+    } catch (error) {
+        console.error("Error connecting to DB:", error)
+    }
+}
+
+export default connectDB
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,12 +1,16 @@
-import "dotenv/config"
+import dotenv from "dotenv"
 import express from "express"
 
+import connectDB from "./config/db.config.js"
+
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5100
 
 app.listen(port, () => {
     console.log(`Auth service is running on port ${port}`)
+    connectDB()
 })
