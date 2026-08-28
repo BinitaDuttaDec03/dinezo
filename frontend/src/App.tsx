4b986e6a23212de37b365aa8a35e812aea2a6f89
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-08-28 22:13:29 +0530

    frontend: - protected and public routes created to protect the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import PublicRoute from "./components/publicRoute";
+import ProtectedRoute from "./components/protectedRote";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter >
     </>
