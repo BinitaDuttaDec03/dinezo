d1ab3c9b6acd0a8047955a4b5ce0783dff6eecf7
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-03 00:05:04 +0530

    utils service: - cloudinary setup

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 import cors from "cors";
 
 dotenv.config();
@@ -8,7 +9,20 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY } = process.env;
+
+if (!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY) {
+  throw new Error("Missing Cloudinary environment variables");
+}
+
+cloudinary.v2.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_SECRET_KEY,
+});
 
 const PORT = process.env.PORT || 5002;
 
