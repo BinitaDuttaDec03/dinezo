033fc6b0b0c692b602cd52b0373d76f6b740f5be
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-12 18:36:43 +0530

    restaurant service: - open/close restaurant status route update - update restaurant route added

--- services/restaurant/src/routes/restaurant.route.ts
+++ services/restaurant/src/routes/restaurant.route.ts
@@ -3,6 +3,8 @@ import express from "express";
 import {
     addRestaurant,
     fetchMyRestaurant,
+    updateRestaurant,
+    updateStatusRestaurant,
 } from "../controllers/restaurant.controller.js";
 import { isAuth, isSeller } from "../middlewares/isAuth.middleware.js";
 import uploadFile from "../middlewares/multer.middleware.js";
@@ -11,5 +13,7 @@ const router = express.Router();
 
 router.post("/new", isAuth, isSeller, uploadFile, addRestaurant);
 router.get("/my", isAuth, isSeller, fetchMyRestaurant);
+router.put("/status", isAuth, isSeller, updateStatusRestaurant);
+router.put("/edit", isAuth, isSeller, updateRestaurant);
 
 export default router;
