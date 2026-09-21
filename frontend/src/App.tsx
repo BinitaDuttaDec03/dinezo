9cb6cfa4cc510c1df29f45b4ec761f0ae0011bff
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-21 23:09:24 +0530

    restaurant service: - cart handling routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Account from "./components/Account";
 import { useAppData } from "./context/AppContext";
 import Restaurant from "./pages/Restaurant";
 import RestaurantPage from "./pages/RestaurantPage";
+import Cart from "./pages/Cart";
 
 const AppContent = () => {
   const { loading, user } = useAppData();
@@ -41,6 +42,7 @@ const AppContent = () => {
           <Route path="/select-role" element={<SelectRole />} />
           <Route path="/account" element={<Account />} />
           <Route path="/restaurant/:id" element={<RestaurantPage />} />
+          <Route path="/cart" element={<Cart />} />
         </Route>
       </Routes>
     </>
--- services/restaurant/src/routes/cart.route.ts
+++ services/restaurant/src/routes/cart.route.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
 import { isAuth } from "../middlewares/isAuth.middleware.js";
-import { addToCart, fetchMyCart } from "../controllers/cart.controller.js";
+import { addToCart, clearCart, decrementCartItem, fetchMyCart, incrementCartItem } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.post("/add", isAuth, addToCart);
 router.get("/all", isAuth, fetchMyCart);
+router.put("/inc", isAuth, incrementCartItem);
+router.put("/dec", isAuth, decrementCartItem);
+router.delete("/clear", isAuth, clearCart);
 
 export default router;
