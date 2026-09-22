de242b1c9738b9d37bf194d43b95a73a33668f4b
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-23 00:09:44 +0530

    restaurant service: - address schema & routes defined

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -6,6 +6,7 @@ import connectDB from "./config/db.config.js";
 import restaurantRoutes from "./routes/restaurant.route.js";
 import itemRoutes from "./routes/menuItem.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import addressRoutes from "./routes/address.route.js";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/api/restaurant", restaurantRoutes);
 app.use("/api/item", itemRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
 
 const PORT = process.env.PORT || 5001;
 
