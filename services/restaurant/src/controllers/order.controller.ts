011b0895c0002a526834461c4ec20a96cb343252
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-26 23:03:58 +0530

    restaurant service: - create order function

---
+++ services/restaurant/src/controllers/order.controller.ts
@@ -0,0 +1,161 @@
+import TryCatch from "../middlewares/tryCatch.middleware.js";
+import type { IAuthenticatedRequest } from "../middlewares/isAuth.middleware.js";
+import Address from "../models/Address.model.js";
+import Cart from "../models/Cart.model.js";
+import type { IMenuItem } from "../models/MenuItems.model.js";
+import type { IRestaurant } from "../models/Restaurant.model.js";
+import Restaurant from "../models/Restaurant.model.js";
+import Order from "../models/Order.model.js";
+
+export const createOrder = TryCatch(async (req: IAuthenticatedRequest, res) => {
+    const user = req.user;
+    if (!user) {
+        return res.status(401).json({
+            message: "Unauthorized",
+        });
+    }
+
+    const { paymentMethod, addressId } = req.body;
+
+    if (!addressId) {
+        return res.status(400).json({
+            message: "Address is required",
+        });
+    }
+
+    const address = await Address.findOne({
+        _id: addressId,
+        userId: user._id,
+    });
+
+    if (!address) {
+        return res.status(404).json({
+            message: "Address Not found",
+        });
+    }
+
+    const getDistanceKm = (
+        lat1: number,
+        lon1: number,
+        lat2: number,
+        lon2: number
+    ): number => {
+        const R = 6371;
+        const dLat = ((lat2 - lat1) * Math.PI) / 180;
+        const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos((lat1 * Math.PI) / 180) *
+            Math.cos((lat2 * Math.PI) / 180) *
+            Math.sin(dLon / 2) *
+            Math.sin(dLon / 2);
+
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return +(R * c).toFixed(2);
+    };
+
+    const cartItems = await Cart.find({ userId: user._id })
+        .populate<{ itemId: IMenuItem }>("itemId")
+        .populate<{ restaurantId: IRestaurant }>("restaurantId");
+
+    if (cartItems.length === 0) {
+        return res.status(400).json({ message: "Cart is empty" });
+    }
+
+    const firstCartItem = cartItems[0];
+
+    if (!firstCartItem || !firstCartItem.restaurantId) {
+        return res.status(400).json({
+            message: "Invalid cart data",
+        });
+    }
+
+    const restaurantId = firstCartItem.restaurantId._id;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+
+    if (!restaurant) {
+        return res.status(404).json({
+            message: "No restaurant with this id",
+        });
+    }
+
+    if (!restaurant.isOpen) {
+        return res.status(404).json({
+            message: "Sorry this restaurant is closed for now",
+        });
+    }
+
+    const distance = getDistanceKm(
+        address.location.coordinates[1],
+        address.location.coordinates[0],
+        restaurant.autoLocation.coordinates[1],
+        restaurant.autoLocation.coordinates[0]
+    );
+
+    let subtotal = 0;
+
+    const orderItems = cartItems.map((cart) => {
+        const item = cart.itemId;
+
+        if (!item) {
+            throw new Error("Invalid cart item");
+        }
+
+        const itemTotal = item.price * cart.quantity;
+
+        subtotal += itemTotal;
+
+        return {
+            itemId: item._id.toString(),
+            name: item.name,
+            price: item.price,
+            quantity: cart.quantity,
+        };
+    });
+
+    const deliveryFee = subtotal < 250 ? 49 : 0;
+    const platformFee = 7;
+    const totalAmount = subtotal + deliveryFee + platformFee;
+
+    const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+
+    const [longitude, latitude] = address.location.coordinates;
+
+    const riderAmount = Math.ceil(distance) * 17;
+
+    const order = await Order.create({
+        userId: user._id.toString(),
+        restaurantId: restaurantId.toString(),
+        restaurantName: restaurant.name,
+        riderId: null,
+        distance,
+        riderAmount,
+        items: orderItems,
+        subtotal,
+        deliveryFee,
+        platformFee,
+        totalAmount,
+        addressId: address._id.toString(),
+        deliveryAddress: {
+            formattedAddress: address.formattedAddress,
+            mobile: address.mobile,
+            latitude,
+            longitude,
+        },
+
+        paymentMethod,
+        paymentStatus: "pending",
+        status: "placed",
+        expiresAt,
+    });
+
+    await Cart.deleteMany({ userId: user._id });
+
+    res.json({
+        message: "Order created successfully",
+        orderId: order._id.toString(),
+        amount: totalAmount,
+    });
+});
